1eefb930b68b02a4957e60c29283049868ad9c9e
Ubuntu <ubuntu@ip-172-31-45-193.ap-northeast-2.compute.internal>
2023-04-24 14:04:20 +0000

    20230424 second commit

--- implementations/esrgan/cpp/esrgan.cpp
+++ implementations/esrgan/cpp/esrgan.cpp
@@ -35,69 +35,36 @@ int main(int argc, const char* argv[]) {
     return -1;
   }
 
+    // Load the Torch model
+    torch::jit::script::Module module;
+    try {
+        module = torch::jit::load(argv[1]);
+    }
+    catch (const c10::Error& e) {
+        std::cerr << "Error loading the model\n";
+        return -1;
+    }
 
-  torch::jit::script::Module module;
-  try {
-    // Deserialize the ScriptModule from a file using torch::jit::load().
-    module = torch::jit::load(argv[1]);
-  }
-  catch (const c10::Error& e) {
-    std::cerr << "error loading the model\n";
-    return -1;
-  }
-  // Create a vector of inputs.
-    std::vector<torch::jit::IValue> inputs;
-    inputs.push_back(torch::ones({8, 3, 64, 64}).to(torch::kCUDA));
+    // Load the input image using OpenCV
+    cv::Mat image1 = cv::imread("/home/ubuntu/PyTorch-GAN/data/img_align_celeba/000001.jpg", cv::IMREAD_COLOR);
 
-    Mat image;
-    image = imread("/home/ubuntu/PyTorch-GAN/data/img_align_celeba/000001.jpg", IMREAD_COLOR);
+    cv::Mat image;
+    cv::Size size(64, 64);  // define the new size of the image
+    cv::resize(image1, image, size);  // resize the image
+    cv::cvtColor(image, image, cv::COLOR_BGR2RGB);
 
+    // Convert the input image to a Torch tensor
+    torch::Tensor input_tensor = torch::from_blob(image.data, {1, image.rows, image.cols, 3}, torch::kFloat);
+    input_tensor = input_tensor.permute({0, 3, 1, 2}).to(torch::kCUDA);
 
-    // Convert the image to a 3D tensor with dimensions (C, H, W)
-    cv::Mat image_float;
-    image.convertTo(image_float, CV_32F, 1.0/255.0);
-    cv::Mat image_transposed = image_float.t();
-    std::vector<int64_t> dims = {1, 3, image_transposed.rows, image_transposed.cols};
-    torch::Tensor input_tensor = torch::from_blob(image_transposed.data, torch::IntList(dims));
+    // Run inference on the input tensor
+    at::Tensor output_tensor = module.forward({input_tensor}).toTensor();
 
-    // Normalize the tensor to match the mean and standard deviation used during training
-    input_tensor[0][0] = (input_tensor[0][0] - 0.485) / 0.229;
-    input_tensor[0][1] = (input_tensor[0][1] - 0.456) / 0.224;
-    input_tensor[0][2] = (input_tensor[0][2] - 0.406) / 0.225;
+    // // Convert the output tensor to a cv::Mat object
+    cv::Mat output_image(output_tensor.size(2), output_tensor.size(3), CV_32FC3, output_tensor.data_ptr<float>());
+    // output_image *= 255.0;
 
-    // Convert the tensor to a variable and move it to the device (CPU or GPU)
-    torch::Tensor input_variable = torch::autograd::make_variable(input_tensor, false);
-    input_variable = input_variable.to(at::kCUDA);
-
-    // Forward pass through the network
-    at::Tensor output = module.forward({input_variable}).toTensor();
-    // Get the current time after executing the code
-
-    // Compute the elapsed time in milliseconds
-
-    // std::cout << output.slice(/*dim=*/1, /*start=*/0, /*end=*/5) << '\n';
-
-    // Convert the output tensor to a CPU tensor and detach it from the computation graph
-    at::Tensor output_cpu = output.to(at::kCPU).detach();
-
-    // Convert the tensor to a 3D CxHxW array
-    std::vector<int64_t> dim = output_cpu.sizes();
-    int C = dim[1];
-    int H = dim[2];
-    int W = dim[3];
-    float* data = output_cpu.data<float>();
-    cv::Mat output_image(H, W, CV_32FC(C), data);
-
-    // Convert the output image from float to unsigned char (0-255)
-    output_image.convertTo(output_image, CV_8UC(C), 255.0);
-
-    // Convert the output image from CxHxW to HxWxC format
-    cv::Mat output_image_transposed = output_image.t();
-    cv::Mat output_image_swapped;
-    cv::cvtColor(output_image_transposed, output_image_swapped, cv::COLOR_BGR2RGB);
-
-    // Save the output image to a file
-    cv::imwrite("output.jpg", output_image_swapped);
+    // Display the output image using OpenCV
 
   std::cout << "ok\n";
 }
